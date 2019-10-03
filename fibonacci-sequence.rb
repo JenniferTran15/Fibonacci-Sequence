b730e02c37db36162d369dc9138105b98be57cd0
Jennifer <jennifertran135@gmail.com>
2019-10-03 06:16:51 +0000

    Fibonacci Sequence

--- fibonacci-sequence.rb
+++ fibonacci-sequence.rb
@@ -1,1 +1,21 @@
-hi
+def recursive_fib(num)
+  return num if num < 2
+  return recursive_fib(num - 1) + recursive_fib(num - 2)
+end
+
+def iterative_fib(num)
+  sequence = [0,1]
+  if num < 2
+    sequence << num
+  else
+    sequence << sequence[-1] + sequence[-2]
+  end
+  sequence.last
+end
+
+require 'benchmark'
+num = 35
+Benchmark.bm do |x|
+  x.report("recursive_fib") { recursive_fib(num) }
+  x.report("iterative_fib")  { iterative_fib(num)  }
+end
